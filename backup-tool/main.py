eb081565537b1c368cb289f02f5ac666177240bc
Thomasderguetige <baumki200025@sr.htlweiz.at>
2024-09-25 12:38:27 +0200

    cli

--- backup-tool/main.py
+++ backup-tool/main.py
@@ -1,18 +1,47 @@
 import backup
+from sys import argv
+from os import listdir
 
-def create():
-    old = backup.Backup.load_from_file("20240925122027")
+def create(dir: str, prev_backup: str):
+    old = backup.Backup.load_from_file(prev_backup)
     created = backup.Backup("saus", old=old.tree).dump()
 
-def create_initial():
-    created = backup.Backup("saus").dump()
+def create_initial(dir: str):
+    created = backup.Backup(dir).dump()
 
 
-def restore():
-    restore = backup.Restorer(["20240925122027", "20240925122131"])
+def restore(backup_files: list[str]):
+    restore = backup.Restorer(backup_files)
     restore.restore("out")
 
 
 
 if __name__ == "__main__":
-    restore()
+    if len(argv) < 2:
+        print("Invalid argument")
+        exit(1)
+    if argv[1] == "--create":
+        if len(argv) != 4:
+            print("Invalid argument")
+            exit(1)
+        dirname = argv[2]
+        filename = argv[3]
+        create(dirname, filename)
+    elif argv[1] == "--create-initial":
+        if len(argv) != 3:
+            print("Invalid argument")
+            exit(1)
+        dir = argv[2]
+        create_initial(dir)
+    elif argv[1] == "--restore":
+        if len(argv) < 3:
+            print("Invalid argument")
+            exit(1)
+        files = argv[2:]
+        for file in files:
+            if file.endswith("*"):
+                files.remove(file)
+                files.extend([file for file in listdir(".") if file.startswith(file[:-1])])
+        restore(files)
+    else:
+        print("Invalid argument")
